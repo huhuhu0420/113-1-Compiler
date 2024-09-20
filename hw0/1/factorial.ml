d2a6bf14c62fe808d078185fac4dfd37675c8de0
huhuhu0420 <ghostcheng.00@gmail.com>
2024-09-20 16:18:59 +0800

    update tail recursive in q1

--- hw0/1/factorial.ml
+++ hw0/1/factorial.ml
@@ -2,7 +2,15 @@ let rec fact (n: int) : int =
   if n = 0 
     then 1 
     else n * fact (n - 1);;
+
+let fact_tail (n: int) : int =
+  let rec fact_aux (n: int) (acc: int) : int =
+    if n = 0 
+      then acc 
+      else fact_aux (n - 1) (n * acc) in
+    fact_aux n 1;;
   
 let () =
   Printf.printf "%d\n" (fact 5);;
+  Printf.printf "%d\n" (fact_tail 5);;
 
--- hw0/1/nb_bit_pos.ml
+++ hw0/1/nb_bit_pos.ml
@@ -2,5 +2,10 @@ let rec nb_bit_pos (n: int) : int =
   if n = 0 then 0
   else (1 land n) + nb_bit_pos (n lsr 1);;
 
+  let rec nb_bit_pos_tail (n: int) (acc: int) : int =
+    if n = 0 then acc
+    else nb_bit_pos_tail (n lsr 1) (acc + (1 land n));;
+
 let () =
   Printf.printf "%d\n" (nb_bit_pos 11);;
+  Printf.printf "%d\n" (nb_bit_pos_tail 11 0);;
